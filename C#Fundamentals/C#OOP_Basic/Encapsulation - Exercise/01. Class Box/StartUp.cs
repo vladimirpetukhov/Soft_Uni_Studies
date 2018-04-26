8ca85273263a1c2efbdd78ce323e8a900a4a89c8
vladimirpetukhov <vlado_web@abv.bg>
2018-04-26 13:47:55 +0300

    Add Exercises

---
+++ C#Fundamentals/C#OOP_Basic/Encapsulation - Exercise/01. Class Box/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace _01.Class_Box
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            var box = new Box(lenght, width, height);
+
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
---
+++ C#Fundamentals/C#OOP_Basic/Encapsulation - Exercise/02. Class Box Data Validation/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace _02.Class_Box_Data_Validation
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            try
+            {
+                double lenght = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                var box = new Box(lenght, width, height);
+
+                Console.WriteLine(box.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}

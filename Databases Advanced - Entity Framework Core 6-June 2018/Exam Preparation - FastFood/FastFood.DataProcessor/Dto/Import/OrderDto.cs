8e267e45fa3fe80af27acb1afd6868b8efd8668f
vladimirpetukhov <vlado_web@abv.bg>
2018-08-09 10:56:32 +0300

    Exam Preparation - FastFood

---
+++ Databases Advanced - Entity Framework Core 6-June 2018/Exam Preparation - FastFood/FastFood.DataProcessor/Dto/Import/OrderDto.cs
@@ -0,0 +1,28 @@
+﻿namespace FastFood.DataProcessor.Dto.Import
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Xml.Serialization;
+
+    [XmlType("Order")]
+    public class OrderDto
+    {
+        [Required]
+        [XmlElement("Customer")]
+        public string Customer { get; set; }
+
+        [Required]
+        [XmlElement("DateTime")]
+        public string DateTime { get; set; }
+
+        [Required]
+        [XmlElement("Type")]
+        public string Type { get; set; }
+
+        [Required]
+        [XmlElement("Employee")]
+        public string Employee { get; set; }
+
+        [XmlArray("Items")]
+        public ItemOrderDto[] ItemOrderDto { get; set; }
+    }
+}

a34d3232dbd35ba985b13000b534be6fa1c200f2
vladimirpetukhov <vlado_web@abv.bg>
2018-08-09 11:02:18 +0300

    Add Exam Preparation

--- Databases Advanced - Entity Framework Core 6-June 2018/Exam Preparation - FastFood/FastFood.Data/Configuration.cs
+++
@@ -1,7 +0,0 @@
-namespace FastFood.Data
-{
-	public static class Configuration
-	{
-		public static string ConnectionString = @"Server=DESKTOP-8DBTK91\SQLEXPRESS02;Database=FastFood;Trusted_Connection=True";
-	}
-}

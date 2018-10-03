40131e258f25509112ae70d1f99cb3bd89e814dc
vladimirpetukhov <vlado_web@abv.bg>
2018-10-03 12:14:49 +0300

    Add SRC

---
+++ ASP.NET Core and MVC Basic/WEB SERVER - ASYNCHRONOUS PROCESSING-Exercise/SRC/SIS.HTTP/Requests/IHttpRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using SIS.HTTP.Cookies;
+using SIS.HTTP.Enums;
+using SIS.HTTP.Headers;
+using SIS.HTTP.Sessions;
+
+namespace SIS.HTTP.Requests
+{
+    public interface IHttpRequest
+    {
+        string Path { get; }
+
+        string Url { get; }
+
+        Dictionary<string, object> FormData { get; }
+
+        Dictionary<string, object> QueryData { get; }
+
+        IHttpHeaderCollection Headers { get; }
+
+        HttpRequestMethod RequestMethod { get; }
+    }
+}
